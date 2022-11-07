da5ca2b1f8b92fea15b6c1d737c1ee9c945b53d4
ejlee <ejlee@mediance.co.kr>
2022-11-07 17:29:55 +0900

    .

--- vue-login/src/App.vue
+++ vue-login/src/App.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import GoogleLogin from './components/GoogleLogin.vue'
+import { defineComponent } from 'vue'
+
+defineComponent({
+  components: {
+    GoogleLogin
+  },
+  data() {
+    return {
+      google: 'google'
+    }
+  },
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <GoogleLogin socialName="google"/>
 </template>
 
 <style scoped>
