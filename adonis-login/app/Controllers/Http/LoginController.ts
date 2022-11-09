40a0173c97e21045be07378f923021e51f91c3d4
ejlee <ejlee@mediance.co.kr>
2022-11-09 14:39:59 +0900

    cors 허용

--- adonis-login/app/Controllers/Http/LoginController.ts
+++ adonis-login/app/Controllers/Http/LoginController.ts
@@ -3,11 +3,13 @@ import Env from '@ioc:Adonis/Core/Env'
 
 export default class LoginController {
   // 로그인 화면 진입
-  public async getGoogleLoginAction ({ ally }: HttpContextContract) {
+  // google/redirect
+  public async getGoogleLoginAction({ ally }: HttpContextContract) {
     return ally.use('google').redirect()
   }
 
   // 토큰 받기
+  // google/callback
   public async getGoogleCallbackAction ({ ally, response }: HttpContextContract) {
     const google = ally.use('google')
 
@@ -26,7 +28,8 @@ export default class LoginController {
     const googleToken = await ally.use('google').accessToken()
     const userInfo = await ally.use('google').userFromToken(googleToken.token)
 
-    return response.redirect().withQs({ googleToken, userInfo }).toPath(Env.get('GOOGLE_REDIRECT_URL'))
-    
+    // console.log(userInfo)
+    // return userInfo
+    return response.redirect().withQs({ googleToken, userInfo }).toPath(Env.get('GOOGLE_REDIRECT_URL'))    
   }
 }
