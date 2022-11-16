532f300b053eb025cd0a8f3de0026340fdf69a61
ejlee <ejlee@mediance.co.kr>
2022-11-16 14:50:24 +0900

    페이스북 미완

--- vue-login/src/components/Facebook.vue
+++ vue-login/src/components/Facebook.vue
@@ -1,10 +1,7 @@
 
-<script>
-function checkLoginState() {
-  FB.getLoginStatus(function (response) {
-    statusChangeCallback(response);
-  });
-}
+<script >
+import axios from 'axios';
+
 window.fbAsyncInit = function () {
   FB.init({
     appId: import.meta.env.VITE_FACEBOOK_CLIENT_ID,
@@ -24,11 +21,55 @@ window.fbAsyncInit = function () {
   js.src = "https://connect.facebook.net/en_US/sdk.js";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+
+// var finished_rendering = function () {
+//   console.log("finished rendering plugins");
+//   var spinner = document.getElementById("spinner");
+//   spinner.removeAttribute("style");
+//   spinner.removeChild(spinner.childNodes[0]);
+// }
+// FB.Event.subscribe('xfbml.render', finished_rendering);
+
+
+export default {
+  data() {
+    return {
+      facebookUserInfo: null,
+    }
+  },
+  mounted() {
+    FB.getLoginStatus(async function (response) {
+      await this.getFacebookData(response.authResponse.accessToken)
+    }); 
+  },
+  methods: {
+    async getFacebookData(accessToken) {
+      const info = await axios.get(`https://graph.facebook.com/me?access_token=${accessToken}`)
+      if (info && info.data) {
+        this.facebookUserInfo = data
+      }
+    }
+  }
+}
+
 </script>
 
 <template>
-<div>
-  <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
-  </fb:login-button>
-</div>
+  <div>
+    {{ facebookUserInfo }}
+    <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+    </fb:login-button>
+    <div id="spinner" style="
+            background: #4267b2;
+            border-radius: 5px;
+            color: white;
+            height: 40px;
+            text-align: center;
+            width: 250px;">
+      Loading
+      <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with"
+        data-use-continue-as="true"></div>
+    </div>
+  </div>
 </template>
